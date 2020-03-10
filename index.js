0e3b411e9f7f5f91c9eb47c2c7ce4d79f973c821
Maks <nekrootmaksim@gmail.com>
2020-03-10 21:45:59 +0300

    start pause methods

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 class Timer{
-    constructor(durationInput, startButton, pauseButton){
+    constructor(durationInput, startButton, pauseButton, callbacks){
         this.durationInput = durationInput;
         this.startButton = startButton;
         this.pauseButton = pauseButton;
+        if (callbacks) {
+            this.onStart = callbacks.onStart;
+        }
 
         this.startButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener('click', this.pause);
     }
-    start() {
-        console.log('start');
+    start = () => {
+        if (this.onStart) {
+            this.onStart();
+        }
+        this.tick();
+        this.interval = setInterval(this.tick, 1000);
+        
+    };
+
+    tick = () => {
+        if (this.timeRemaining <= 0) {
+            this.pause();
+        }else {
+            this.timeRemaining = this.timeRemaining - 1;
+        }
+    };
+
+    pause = () => {
+        clearInterval(this.interval);
+    };
+
+    get timeRemaining () {
+        return parseFloat(this.durationInput.value)
+    }
+
+    set timeRemaining (time) {
+        return this.durationInput.value = time;
     }
 }
 
@@ -15,4 +44,12 @@ const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
-const timer = new Timer(durationInput,startButton,pauseButton);
+const timer = new Timer(durationInput,startButton,pauseButton, {
+    onStart() {
+        console.log("timer started!");
+    },
+    onTick(){},
+    onComplete(){}
+});
+
+
